888e323e27294657cc241586563beb95694c1dd2
Gessé Carlos <gesse.dev@outlook.com>
2022-01-31 07:17:54 -0300

    sales router finished

---
+++ controllers/salesRouter.js
@@ -0,0 +1,36 @@
+const Sales = require('express').Router();
+
+const { validateSales:
+  {
+    validateProductId,
+    validateSales,
+    validateProductQuantity,
+  },
+} = require('./middlewares');
+const { add, getAll, getById, update } = require('./productsController');
+
+Sales.post(
+  '/',
+  validateProductId,
+  validateSales,
+  validateProductQuantity,
+  add,
+);
+
+Sales.get(
+  '/',
+  getAll,
+);
+
+Sales.get(
+  '/:id',
+  getById,
+);
+
+Sales.put(
+  '/:id',
+  validateProductId,
+  validateSales,
+  validateProductQuantity,
+  update,
+);
