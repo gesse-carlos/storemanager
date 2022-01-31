56d216c292a5a5902402b0c111d390bf93512b66
Gessé Carlos <gesse.dev@outlook.com>
2022-01-31 07:22:06 -0300

    index js file finished with routes and express finished

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// não remova esse endpoint, e para o avaliador funcionar
+const middlewares = require('./controllers/middlewares');
+const { productsRouter, salesRouter } = require('./controllers');
+
+const app = express();
+app.use(bodyParser.json());
+
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsRouter);
+app.use('/sales', salesRouter);
+
+app.use(middlewares.domainError);
+app.use(middlewares.error);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
