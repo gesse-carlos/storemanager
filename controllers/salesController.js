111750aaa26b9990d7b73624444949376a897257
Gessé Carlos <gesse.dev@outlook.com>
2022-01-31 06:59:17 -0300

    salesController finished with all http handlers functions using salesService

---
+++ controllers/salesController.js
@@ -0,0 +1,45 @@
+const rescue = require('express-rescue');
+
+const salesService = require('../service/salesService');
+
+const add = rescue(async (req, res) => {
+  const sale = await salesService.add(req.body);
+
+  res.status(201).json({
+    id: sale.insertId,
+    itemsSold: req.body,
+  });
+});
+
+const getAll = rescue(async (req, res) => {
+  const sales = await salesService.getAll();
+
+  res.status(200).json(sales);
+});
+
+const getById = rescue(async (req, res) => {
+  const { id } = req.params;
+
+  const sale = await salesService.getById(id);
+
+  res.status(200).json(sale);
+});
+
+const update = rescue(async (req, res) => {
+  const { id } = req.params;
+  const [product] = req.body;
+
+  const sale = await salesService.getById(id);
+  if (!sale) return res.status(404).json({ message: 'Sale not found' });
+
+  await salesService.update(product);
+
+  res.status(200).json({ saleId: id, itemUpdated: req.body });
+});
+
+module.exports = {
+  add,
+  getAll,
+  getById,
+  update,
+};
