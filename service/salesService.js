12a785f487f4a877e58d6f19107b346ab9a2628b
Gessé Carlos <gesse.dev@outlook.com>
2022-01-31 07:40:03 -0300

    i created a updateProductQuantity function to bonus tasks

--- service/salesService.js
+++ service/salesService.js
@@ -1,4 +1,5 @@
 const { salesModel } = require('../models');
+const productsService = require('./productsService');
 
 const add = async (sales) => salesModel.add(sales);
 
@@ -8,8 +9,24 @@ const getById = async (id) => salesModel.getById(id);
 
 const update = async ({ id, quantity }) => salesModel.update(id, quantity);
 
+const updateProductQuantity = async (sales) => {
+  const updateQuantity = sales.map(async (sale) => {
+    const { product_id: id, quantity: saleQuantity } = sale;
+
+    const product = await productsService.getById(id);
+    product.quantity -= saleQuantity;
+
+    await productsService.update(product);
+  });
+  await Promise.all(updateQuantity);
+
+  const createSale = await add(sales);
+
+  return createSale;
+};
+
 module.exports = {
-  add,
+  add: updateProductQuantity,
   getAll,
   getById,
   update,
